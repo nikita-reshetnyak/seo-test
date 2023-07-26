41be078faedf81d8d6a246b20c356f7664bd3e35
Nikita Reshetnyak <reshetnyaknikita6@gmail.com>
2023-07-26 14:18:29 +0300

    refactor: Add all urls

--- src/sitemap.js
+++ src/sitemap.js
@@ -8,6 +8,26 @@
     const urls = [
       { url: '/', changefreq: 'daily', priority: 1 },
       { url: '/list', changefreq: 'daily', priority: 1 },
+      { url: '/card/1', changefreq: 'hourly', priority: 1 },
+      { url: '/card/2', changefreq: 'hourly', priority: 1 },
+      { url: '/card/3', changefreq: 'hourly', priority: 1 },
+      { url: '/card/4', changefreq: 'hourly', priority: 1 },
+      { url: '/card/5', changefreq: 'hourly', priority: 1 },
+      { url: '/card/6', changefreq: 'hourly', priority: 1 },
+      { url: '/card/7', changefreq: 'hourly', priority: 1 },
+      { url: '/card/8', changefreq: 'hourly', priority: 1 },
+      { url: '/card/9', changefreq: 'hourly', priority: 1 },
+      { url: '/card/10', changefreq: 'hourly', priority: 1 },
+      { url: '/card/11', changefreq: 'hourly', priority: 1 },
+      { url: '/card/12', changefreq: 'hourly', priority: 1 },
+      { url: '/card/13', changefreq: 'hourly', priority: 1 },
+      { url: '/card/14', changefreq: 'hourly', priority: 1 },
+      { url: '/card/15', changefreq: 'hourly', priority: 1 },
+      { url: '/card/16', changefreq: 'hourly', priority: 1 },
+      { url: '/card/17', changefreq: 'hourly', priority: 1 },
+      { url: '/card/18', changefreq: 'hourly', priority: 1 },
+      { url: '/card/19', changefreq: 'hourly', priority: 1 },
+      { url: '/card/20', changefreq: 'hourly', priority: 1 },
       // Add additional pages here
     ];
     
